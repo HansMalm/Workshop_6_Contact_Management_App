61b3c66124c64adb311495f06e1ea2567f46a2c2
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-03-12 16:07:58 +0100

    modify main
    add menu skeleton

--- src/main/java/Lexicon/Hans/Main.java
+++ src/main/java/Lexicon/Hans/Main.java
@@ -5,6 +5,13 @@ package Lexicon.Hans;
 public class Main {
     static void main() {
 
+    ContactsMap contactsMap = new ContactsMap();
 
+    while (true) {
+
+        //menu
+
+        switch ()
+    }
     }
 }
