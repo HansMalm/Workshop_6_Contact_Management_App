e52f47c7e4edbbc72ad430d2b4b1119f8e89ec30
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-03-13 09:43:07 +0100

    modify main
    add menu

--- src/main/java/Lexicon/Hans/Main.java
+++ src/main/java/Lexicon/Hans/Main.java
@@ -1,17 +1,34 @@
 package Lexicon.Hans;
 
-
+import java.util.Scanner;
 
 public class Main {
     static void main() {
 
-    ContactsMap contactsMap = new ContactsMap();
+        ContactsMap contactsMap = new ContactsMap();
+        Scanner scanner = new Scanner(System.in);
 
-    while (true) {
+            System.out.println("Welcome to Contact Management!");
+        while (true) {
 
-        //menu
+            System.out.println("""
+                1. Add Contact
+                2. Search by Name
+                3. Display All Contacts
+                0. End App
+                """ + "\nInput: ");
 
-        switch ()
-    }
+            int select = scanner.nextInt();
+
+            switch (select) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 0:
+            }
+        }
     }
 }
